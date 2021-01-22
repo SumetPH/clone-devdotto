5cc8a31b8666ba1be88e8f4fcba783e98d75e20b
sumetph <notsumet1@gmail.com>
2021-01-22 14:41:30 +0700

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="bg-gray-700">
+    <div class="container mx-auto text-white py-2 xl:px-36">
+      <div class="grid grid-cols-2">
+        <div>
+          <div class="inline-block py-2 px-1 bg-white rounded text-black">
+            <span class="font-bold text-xl">DEV</span>
+          </div>
+          <input
+            class="w-80 hidden md:inline-block bg-gray-600 rounded ml-2 p-2 focus:outline-none focus:ring-2 focus:ring-blue-700"
+            type="text" placeholder="Search..." />
+        </div>
+
+        <div class="flex justify-end items-center space-x-3">
+          <a class="hidden md:inline-block bg-purple-500 rounded p-2 text-black" href="#">Write a post</a>
+          <div>
+            <svg class="w-8 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+            </svg>
+          </div>
+          <div>
+            <svg class="w-8 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+            </svg>
+          </div>
+          <div>
+            <img class="w-8 rounded-full inline-block"
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s---cZ17P-H--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/474923/a35b0796-fe23-4a48-b648-6b85781952d4.jpeg"
+              alt="">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
+  <div class="bg-gray-800 h-52"></div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,140 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-800 text-white">
+    <div class="container mx-auto xl:px-36  pt-2">
+      <div class="grid grid-cols-4 gap-4">
+        <ul class="hidden md:block">
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Home</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Reading
+              List</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Listings</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Podcasts</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Videos</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300" href="#">Tags</a>
+          </li>
+          <li>
+            <a class="inline-block w-full p-2 rounded hover:bg-gray-700 hover:text-purple-300 text-sm"
+              href="#">More...</a>
+          </li>
+        </ul>
+
+        <div class="col-span-4 md:col-span-3 lg:col-span-2">
+          <!-- header content -->
+          <div class="grid grid-cols-3 mb-3">
+            <div class="flex items-center">
+              <span class="inline-block font-bold text-xl pl-3 md:pl-0">Posts</span>
+            </div>
+            <div class="hidden md:block col-span-2 flex justify-end items-center text-gray-300">
+              <a class="border-b-2 border-blue-500 p-2" href="#">Feed</a>
+              <a class="p-2" href="#">Week</a>
+              <a class="p-2" href="#">Month</a>
+              <a class="p-2" href="#">Year</a>
+              <a class="p-2" href="#">Infinity</a>
+              <a class="p-2" href="#">Latest</a>
+            </div>
+          </div>
+          <!-- end header content -->
+
+          <!-- content -->
+          <div class="w-full bg-gray-700 mb-2 rounded-lg overflow-hidden">
+            <img class="object-cover"
+              src="https://res.cloudinary.com/practicaldev/image/fetch/s--M8enUJ3l--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/yb1nc85cjryewmxxsgcd.png"
+              alt="">
+            <div class="p-5">
+              <div class="flex mb-2">
+                <div>
+                  <img class="w-8 rounded-full"
+                    src="https://res.cloudinary.com/practicaldev/image/fetch/s--yLe7MHLr--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/429862/46d4537e-4efc-4333-b743-d0f67136bb57.png"
+                    alt="">
+                </div>
+                <div class="ml-2 text-xs text-gray-300">
+                  <p>Unnati Bamania </p>
+                  <p>Jan 18</p>
+                </div>
+              </div>
+              <div>
+                <router-link to="/content" class="font-bold text-3xl mb-2">Some projects to build your resume!
+                </router-link>
+                <div class="space-x-3 mb-2">
+                  <span>#javascript</span>
+                  <span>#react</span>
+                  <span>#web</span>
+                </div>
+                <div class="space-x-5">
+                  <span>700 reactions</span>
+                  <span>600 comments</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="w-full bg-gray-700 mb-2 rounded-lg overflow-hidden">
+            <div class="p-5">
+              <div class="flex mb-2">
+                <div>
+                  <img class="w-8 rounded-full"
+                    src="https://res.cloudinary.com/practicaldev/image/fetch/s--OIZ_tbLk--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/137579/7c12095e-3ceb-4e71-9845-79ac31a72dab.jpg                     "
+                    alt="">
+                </div>
+                <div class="ml-2 text-xs text-gray-300">
+                  <p>Charlotte Fereday </p>
+                  <p>Jan 17</p>
+                </div>
+              </div>
+              <div>
+                <p class="font-bold text-3xl mb-2">Authentication and Authorisation 101</p>
+                <div class="space-x-3 mb-2">
+                  <span>#javascript</span>
+                  <span>#react</span>
+                  <span>#web</span>
+                </div>
+                <div class="space-x-5">
+                  <span>500 reactions</span>
+                  <span>400 comments</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end content -->
+        </div>
+
+        <div class="hidden lg:block">
+          <div class="bg-gray-700 rounded-md">
+            <div class="flex justify-between items-end p-4">
+              <span class="text-xl font-bold">Listings</span>
+              <a href="#" class="text-sm text-blue-500">See all</a>
+            </div>
+            <a href="#">
+              <div class="hover:bg-gray-800 p-4">
+                <p class="mb-1">Build a full-stack role-based auth in 60 mins</p>
+                <p class="text-sm">education</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="hover:bg-gray-800 p-4">
+                <p class="mb-1">Full-stack role-based authorization in 60 mins</p>
+                <p class="text-sm">education</p>
+              </div>
+            </a>
+            <a href="#">
+              <div class="hover:bg-gray-800 p-4">
+                <p class="mb-1">Free Global Diversity CFP Day virtual workshop on Saturday</p>
+                <p class="text-sm">education</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
